4ed21397d064e7da843013b1d3be3afe4128ebf1
Christoph Walcher <christoph-wa@gmx.de>
2019-11-26 22:14:50 +0100

    Overengineer stuff

--- app/src/main/java/de/wiomoc/miocheck/LockerFragment.kt
+++ app/src/main/java/de/wiomoc/miocheck/LockerFragment.kt
@@ -11,10 +11,6 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.github.mikephil.charting.formatter.ValueFormatter
-import com.github.mikephil.charting.utils.ColorTemplate
-import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import kotlinx.android.synthetic.main.fragment_locker.*
 import org.koin.android.ext.android.inject
 import java.text.SimpleDateFormat
@@ -34,8 +30,8 @@ class LockerFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_locker, container, false)
     }
@@ -44,11 +40,11 @@ class LockerFragment : Fragment() {
         super.onStart()
 
         val takeButton = locker_take_button
-        lockerService.subscribeBalanceChange {
+        lockerService.subscribeBalanceChange(this) {
             view!!.findViewById<TextView>(R.id.locker_account_credit_txt).text = it.toString()
         }
 
-        lockerService.subscribeInventoryChange {
+        lockerService.subscribeInventoryChange(this) {
             view!!.findViewById<TextView>(R.id.locker_available_txt).text = it.toString()
             takeButton.isEnabled = it > 0
         }
@@ -62,55 +58,39 @@ class LockerFragment : Fragment() {
         }
 
         locker_history_chart.apply {
-            val list = mutableListOf<Entry>()
+            setViewPortOffsets(0f, 0f, 0f, 0f)
+            lockerService.subscribeHistoryChange(this@LockerFragment) { history ->
+                val dataSet = LineDataSet(
+                    history.map { Entry(it.timestamp.toFloat(), it.inventory.toFloat()) },
+                    getString(R.string.locker_available)
+                )
 
-            val lineDataSet = LineDataSet(mutableListOf(), getString(R.string.locker_available))
-
-            lockerService.history().addChildEventListener(object : ChildEventListener {
-                override fun onCancelled(p0: DatabaseError) {
+                dataSet.apply {
+                    mode = LineDataSet.Mode.HORIZONTAL_BEZIER;
+                    fillAlpha = 255;
+                    fillColor = resources.getColor(R.color.colorAccent);
+                    setDrawFilled(true)
+                    setDrawValues(false)
+                    color = fillColor
+                    setDrawCircles(false)
                 }
 
-                override fun onChildMoved(p0: DataSnapshot, p1: String?) {
-                }
+                data = LineData(dataSet)
+                invalidate()
 
-                override fun onChildChanged(p0: DataSnapshot, p1: String?) {
-                }
+                data.isHighlightEnabled = false
+            }
+            description = null
 
-                override fun onChildAdded(p0: DataSnapshot, p1: String?) {
-                    val timestamp = p0.child("timestamp").getValue(Long::class.java)!!
-                    val inventory = p0.child("newInventory").getValue(Long::class.java)!!
-
-                    println("$timestamp - $inventory")
-
-                    list.add(Entry(timestamp.toFloat(), inventory.toFloat()))
-                    data.dataSets[0] = LineDataSet(list, getString(R.string.locker_available))
-                    data.notifyDataChanged()
-                }
-
-                override fun onChildRemoved(p0: DataSnapshot) {
-                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-                }
-
-            })
-
-            data = LineData(lineDataSet)
-
-            description.isEnabled = false
-
-            // enable touch gestures
             setTouchEnabled(true)
 
             dragDecelerationFrictionCoef = 0.9f
 
-            // enable scaling and dragging
             isDragEnabled = true
             setScaleEnabled(true)
             setDrawGridBackground(false)
             isHighlightPerDragEnabled = true
 
-            // set an alternative background color
-            setBackgroundColor(Color.WHITE)
-            setViewPortOffsets(0f, 0f, 0f, 0f)
 
             // get the legend (only possible after setting data)
             legend.isEnabled = false
@@ -119,13 +99,13 @@ class LockerFragment : Fragment() {
             xAxis.textSize = 10f
             xAxis.textColor = Color.WHITE
             xAxis.setDrawAxisLine(false)
-            xAxis.setDrawGridLines(true)
-            xAxis.textColor = Color.rgb(255, 192, 56)
+            xAxis.setDrawGridLines(false)
+            xAxis.textColor = Color.BLACK
             xAxis.setCenterAxisLabels(true)
-            xAxis.granularity = 1f // one hour
+            xAxis.granularity = 1f
 
             xAxis.valueFormatter = object :
-                    ValueFormatter() {
+                ValueFormatter() {
                 private val format = SimpleDateFormat("dd MMM HH:mm")
 
                 override fun getFormattedValue(value: Float): String {
@@ -135,13 +115,11 @@ class LockerFragment : Fragment() {
 
             val leftAxis = axisLeft
             leftAxis.setPosition(YAxis.YAxisLabelPosition.INSIDE_CHART)
-            leftAxis.textColor = ColorTemplate.getHoloBlue()
             leftAxis.setDrawGridLines(true)
             leftAxis.isGranularityEnabled = true
             leftAxis.axisMinimum = 0f
-            leftAxis.yOffset = -9f
-            leftAxis.textColor = Color.rgb(255, 192, 56)
-            leftAxis.setDrawLabels(false)
+            leftAxis.textColor = Color.BLACK
+            leftAxis.yOffset = -5f
 
             axisRight.isEnabled = false
         }
@@ -150,12 +128,12 @@ class LockerFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.fragment_locker, menu)
         menu.findItem(R.id.fragment_locker_alarm)
-                .setIcon(
-                        if (notificationService.hasSubscribedToTopic(INVENTORY_LOW_TOPIC))
-                            R.drawable.ic_alarm_off
-                        else
-                            R.drawable.ic_alarm_add
-                )
+            .setIcon(
+                if (notificationService.hasSubscribedToTopic(INVENTORY_LOW_TOPIC))
+                    R.drawable.ic_alarm_off
+                else
+                    R.drawable.ic_alarm_add
+            )
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
--- app/src/main/java/de/wiomoc/miocheck/LockerService.kt
+++ app/src/main/java/de/wiomoc/miocheck/LockerService.kt
@@ -1,16 +1,54 @@
 package de.wiomoc.miocheck
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.*
 import com.google.firebase.functions.FirebaseFunctions
 
 class LockerService(
-        private val dbReference: DatabaseReference,
-        private val functions: FirebaseFunctions
+    private val dbReference: DatabaseReference,
+    private val functions: FirebaseFunctions
 ) {
+
+    class LifecycleAwareValueEventListener private constructor(
+        private val query: Query,
+        private val valueEventListener: ValueEventListener
+    ) :
+        LifecycleObserver {
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_START)
+        fun onStart() {
+            query.addValueEventListener(valueEventListener)
+        }
+
+        @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+        fun onStop() {
+            query.removeEventListener(valueEventListener)
+        }
+
+        companion object {
+            fun start(lifecycleOwner: LifecycleOwner, query: Query, cb: (snapshot: DataSnapshot) -> Unit) {
+                val listener = LifecycleAwareValueEventListener(query, object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError) {}
+
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        cb(snapshot)
+                    }
+                })
+
+                lifecycleOwner.lifecycle.let {
+                    if (it.currentState == Lifecycle.State.STARTED) {
+                        listener.onStart()
+                    }
+                    it.addObserver(listener)
+                }
+            }
+        }
+    }
+
     fun addMio() {
         functions.getHttpsCallable("mioTransaction").call(mapOf("change" to 1))
     }
@@ -19,34 +57,48 @@ class LockerService(
         functions.getHttpsCallable("mioTransaction").call(mapOf("change" to -1))
     }
 
-    fun subscribeInventoryChange(cb: ((Long) -> Unit)) = dbReference
-            .child("locker")
-            .child("inventory")
-            .addValueEventListener(object : ValueEventListener {
-                override fun onCancelled(p0: DatabaseError) {}
-                override fun onDataChange(p0: DataSnapshot) {
-                    cb(p0.value as Long)
-                }
-            })
+    fun subscribeInventoryChange(owner: LifecycleOwner, cb: ((Long) -> Unit)) {
+        LifecycleAwareValueEventListener.start(
+            owner,
+            dbReference
+                .child("locker")
+                .child("inventory")
+        ) {
+            cb(it.value as Long)
+        }
+    }
 
-    fun subscribeBalanceChange(cb: ((Long) -> Unit)) =
-            FirebaseAuth.getInstance().currentUser?.let { user ->
+
+    fun subscribeBalanceChange(owner: LifecycleOwner, cb: ((Long) -> Unit)) {
+        FirebaseAuth.getInstance().currentUser?.let { user ->
+            LifecycleAwareValueEventListener.start(
+                owner,
                 dbReference
-                        .child("account")
-                        .child(user.uid)
-                        .child("balance")
-                        .addValueEventListener(object : ValueEventListener {
-                            override fun onCancelled(p0: DatabaseError) {}
-
-                            override fun onDataChange(p0: DataSnapshot) {
-                                cb((p0.value ?: 0L) as Long)
-                            }
-                        })
+                    .child("account")
+                    .child(user.uid)
+                    .child("balance")
+            ) {
+                cb((it.value ?: 0L) as Long)
             }
+        }
 
-    fun history() = dbReference
-            .child("history")
-            .limitToLast(50)
-            .orderByChild("timestamp")
+    }
 
+    data class HistoryEntry(val timestamp: Long, val inventory: Long)
+
+    fun subscribeHistoryChange(owner: LifecycleOwner, cb: (List<HistoryEntry>) -> Unit) {
+        LifecycleAwareValueEventListener.start(
+            owner,
+            dbReference
+                .child("history")
+                .limitToLast(50)
+                .orderByChild("timestamp")
+        ) {
+            cb(it.children.map {
+                val timestamp = it.child("timestamp").getValue(Long::class.java)!!
+                val inventory = it.child("newInventory").getValue(Long::class.java)!!
+                HistoryEntry(timestamp, inventory)
+            })
+        }
+    }
 }
